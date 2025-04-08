7a3e4a848b51fdc39b0e02bd3aef67da0d5584c1
samueltobler <47925314+samueltobler@users.noreply.github.com>
2025-04-08 13:32:09 +0200

    altruismus v4

--- shinyCorrect.R
+++ shinyCorrect.R
@@ -17,14 +17,17 @@ ui <- fluidPage(
     
     selectInput(
       "selected_question",
-      "Wählen Sie die Frage aus:",
+      "Wählen Sie die Aufgabe aus:",
       choices = c(
-        "Kapitel 3.1: Frage 1",
-        "Kapitel 3.1: Frage 2",
-        "Kapitel 3.1: Frage 3",
-        "Kapitel 3.2: Frage 1",
-        "Kapitel 3.2: Frage 2",
-        "Kapitel 3.2: Frage 3"
+        "Kapitel 3.1: Aufgabe 1",
+        "Kapitel 3.1: Aufgabe 2",
+        "Kapitel 3.1: Aufgabe 3",
+        "Kapitel 3.2: Aufgabe 1",
+        "Kapitel 3.2: Aufgabe 2",
+        "Kapitel 3.2: Aufgabe 3",
+        "Kapitel 3.3: Aufgabe 1",
+        "Kapitel 3.3: Aufgabe 2",
+        "Kapitel 3.4: Aufgabe 1"
       )
     ),
     
@@ -50,31 +53,42 @@ server <- function(input, output, session) {
   
   user_text <- eventReactive(input$go_button, {
     
-    if(input$selected_question == "Kapitel 3.1: Frage 1") {
-      instruction = paste(instruct.general, "Erklären Sie die Formel $r=0.5^n$ in eigenen Worten. ")
+    if(input$selected_question == "Kapitel 3.1: Aufgabe 1") {
+      instruction = paste(instruct.general, "Erklären Sie die Formel r=0.5^n in eigenen Worten. ")
     } 
-    if(input$selected_question == "Kapitel 3.1: Frage 2") {
-      instruction = paste(instruct.general, "Berechnen Sie den Verwandtschaftskoeffizient $r$ für Individuen 2 und 4, sowie Individuen 3 und 4. Die richtige Lösung wäre: Für Individuen 2 und 4; r wäre = 2*(0.5)^3 = 0.25, für 3 und 4: r = 2*0.5^4 = 0.125")
+    if(input$selected_question == "Kapitel 3.1: Aufgabe 2") {
+      instruction = paste(instruct.general, "Berechnen Sie den Verwandtschaftskoeffizient r für Individuen 2 und 4, sowie Individuen 3 und 4. Die richtige Lösung wäre: Für Individuen 2 und 4; r wäre = 2*(0.5)^3 = 0.25, für 3 und 4: r = 2*0.5^4 = 0.125")
     } 
-    if(input$selected_question == "Kapitel 3.1: Frage 3") {
+    if(input$selected_question == "Kapitel 3.1: Aufgabe 3") {
       instruction = paste(instruct.general, "Berechnen Sie r für Individuen 4 und 5. Die richtige Lösung wäre: 2*0.5^3+0.5 = 0.75")
     } 
     
-    if(input$selected_question == "Kapitel 3.2: Frage 1") {
+    if(input$selected_question == "Kapitel 3.2: Aufgabe 1") {
       instruction = paste(instruct.general, "Die Kosten dieses der Wächterin Verhaltens könnten nicht höher sein. Erklären Sie mögliche Nutzen dieses Verhaltens im Kontext des Optimalitätsprinzips.")
     } 
-    if(input$selected_question == "Kapitel 3.2: Frage 2") {
+    if(input$selected_question == "Kapitel 3.2: Aufgabe 2") {
       instruction = paste(instruct.general, "Das Verhalten der Wächterin wird oft als «altruistisch» bezeichnet. Stellen Sie eine Definition für dafür auf. Beziehen Sie sich dabei auf ultimate Verhaltensursachen. ")
     } 
-    if(input$selected_question == "Kapitel 3.2: Frage 3") {
+    if(input$selected_question == "Kapitel 3.2: Aufgabe 3") {
       instruction = paste(instruct.general, "Überlegen Sie sich, von welchen Faktoren «Fitness» im evolutiven Kontext abhängt. Was ist der Einfluss des altruistischen Verhaltens der Arbeiterinnen auf die Fitness?")
     } 
+    if(input$selected_question == "Kapitel 3.3: Aufgabe 1") {
+      instruction = paste(instruct.general, " Unter Anwendung von Hamilton' Regel, welches Tier hat eine höhere Fitness, eines das auf 2 eigene Nachkommen verzichtet, dafür einem Geschwister hilft, 5 Nachkommen zu produzieren, oder ein Tier, das nicht hilft? (Annahme: diploide Art)")
+    } 
+    if(input$selected_question == "Kapitel 3.3: Aufgabe 2") {
+      instruction = paste(instruct.general, "Unter Anwendung von Hamilton' Regel, welches Tier hat eine höhere Fitness, eines das auf 3 eigene Nachkommen verzichtet, dafür einem Geschwister hilft, 5 Nachkommen zu produzieren, oder ein Tier, das nicht hilft? (Annahme: diploide Art)")
+    } 
+    
+    if(input$selected_question == "Kapitel 3.4: Aufgabe 1") {
+      instruction = paste(instruct.general, "Welche weiteren Formen nebst der Verwandtenselektion könnten Altruismus als evolutionär stabile Strategie etablieren? ")
+    } 
+    
     
     withProgress(message = 'Antwort wird evaluiert, bitte warten.', style = "notification", value = 0,  {
       incProgress(0.1)
-    gpt_chat <- ellmer::chat_openai(system_prompt = instruction, api_key = apiKey, echo="text", model = gpt_model)
-    reply <- gpt_chat$chat(input$text_input)
-    incProgress(1)
+      gpt_chat <- ellmer::chat_openai(system_prompt = instruction, api_key = apiKey, echo="text", model = gpt_model)
+      reply <- gpt_chat$chat(input$text_input)
+      incProgress(1)
     })
     paste0(reply)
   })
